a424a5cbb342bf7731697a6150fe79c73edb3dad
Suraj chauhan <surajssc1232@gmail.com>
2024-12-25 22:25:06 +0530

    idk

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-# Update package list and install dependencies
-sudo apt-get update
-sudo apt-get install -y openjdk-17-jdk maven
+# Install dependencies using apt-get without sudo
+apt-get update
+apt-get install -y openjdk-17-jdk maven
 
 # Set JAVA_HOME
 export JAVA_HOME="/usr/lib/jvm/java-17-openjdk-amd64"
